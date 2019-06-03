5ae79f39adc36b5b6da122a67b0d95a6f941e34e
skyline25 <skyline25@yandex.ru>
2019-06-03 11:42:55 +0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## React-Redux project
-
-This is experimental test project. Main goal of it is - learn react main features.
---
+++ precache-manifest.0b079072a4276698adc6c27106993004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "0831664c408b9352dfd0",
+    "url": "/shop/static/js/main.07ee5309.chunk.js"
+  },
+  {
+    "revision": "7c2ef3fbecb0c8f50ad8",
+    "url": "/shop/static/js/2.9fa02abc.chunk.js"
+  },
+  {
+    "revision": "0831664c408b9352dfd0",
+    "url": "/shop/static/css/main.b369b02b.chunk.css"
+  },
+  {
+    "revision": "d1d5983e74548a7759df9ac6840dd2f9",
+    "url": "/shop/index.html"
+  }
+];
